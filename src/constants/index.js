74a3094f836746c14c5641c69a27f3c7f0203bd8
VAMSHIDHAR DAWOOR <vamshidhar.20bce7285@vitap.ac.in>
2024-01-05 12:48:11 +0530

    Projects page Updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -59,3 +59,41 @@ export const certificates = [
     org: 'MongoDB',
   },
 ];
+
+export const projects = [
+  {
+    title: 'E-Commerce website',
+    client: 'NR Kids',
+    link: 'https://github.com/vamshidhar644/nrkids',
+    image: 'nrkids.jpg',
+    desc: 'Developed a solo E-commerce website using MERN stack in 6 months. Improved site architecture, reducing load times. Seamlessly integrated secure payment gateways. Enhanced UI/UX, resulting in higher conversions and lower mobile bounce rates. Achieved a high uptime rate of 99.99%. Increased organic traffic, boosting online visibility for NRKids Clothing E-Commerce.',
+  },
+  {
+    title: 'Lost & Found web portal',
+    client: 'VIT-AP',
+    link: 'https://github.com/vamshidhar644/Lost_and_Found',
+    image: 'lostfound.png',
+    desc: "I'm currently developing an intuitive application to streamline lost and found management for 3500+ university students. It simplifies item reporting, reduces resolution time, and lowers unclaimed items, promoting a cleaner campus. Real-time updates enhance user satisfaction, and analytics provide valuable insights for reducing lost item incidents.",
+  },
+  {
+    title: 'CSV to GoogleSheets Importer',
+    link: 'https://github.com/vamshidhar644/CSV_Importer',
+    client: 'Frontend - React.js',
+    image: 'csv.png',
+    desc: 'Introducing the CSV Importer for Google Sheets: A game-changer for data analysts. With drag-and-drop ease, it imports CSV files, lets you choose columns, and even apply filters before import. Say goodbye to tedious data structuring and hello to efficient insights extraction. Boost your productivity and streamline your workflow with our user-friendly tool.',
+  },
+  {
+    title: 'Library Management',
+    link: 'https://github.com/vamshidhar644/library_management',
+    client: 'Backend - Node, Express',
+    image: 'library.png',
+    desc: 'Created a secure library management system with user registration, admin access, and book management using NodeJS and MySQL. Developed APIs for adding books, searching, checking availability, and booking with authentication. Ensured admin endpoints are protected with a secret key. System is tested, documented, and deployed for college library operations.',
+  },
+  {
+    title: 'Quizzy App',
+    link: 'https://github.com/vamshidhar644/quizzy-mernstack',
+    client: 'MERN Stack',
+    image: 'quizzy.jpg',
+    desc: "As the team lead for Quizzy, I spearheaded the development of our MERN stack quiz app, and we executed it excellently. The project was a resounding success, reflecting our team's dedication and delivering a high-quality product. I'm incredibly proud of our achievement.",
+  },
+];
--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -1,64 +1,40 @@
 import React from 'react';
 import './Projects.css';
 import { Link } from 'react-router-dom';
+import { projects } from '../../constants';
 
 const Projects = () => {
-  const projects = [
-    {
-      title: 'E-Commerce website',
-      client: 'NR Kids',
-      link: 'https://github.com/vamshidhar644/nrkids',
-      image: 'nrkids.jpg',
-      desc: 'Developed a solo E-commerce website using MERN stack in 6 months. Improved site architecture, reducing load times. Seamlessly integrated secure payment gateways. Enhanced UI/UX, resulting in higher conversions and lower mobile bounce rates. Achieved a high uptime rate of 99.99%. Increased organic traffic, boosting online visibility for NRKids Clothing E-Commerce.',
-    },
-    {
-      title: 'Lost & Found web portal',
-      client: 'VIT-AP',
-      link: 'https://github.com/vamshidhar644/Lost_and_Found',
-      image: 'lostfound.png',
-      desc: "I'm currently developing an intuitive application to streamline lost and found management for 3500+ university students. It simplifies item reporting, reduces resolution time, and lowers unclaimed items, promoting a cleaner campus. Real-time updates enhance user satisfaction, and analytics provide valuable insights for reducing lost item incidents.",
-    },
-    {
-      title: 'CSV to GoogleSheets Importer',
-      link: 'https://github.com/vamshidhar644/CSV_Importer',
-      client: 'Frontend - React.js',
-      image: 'csv.png',
-      desc: 'Introducing the CSV Importer for Google Sheets: A game-changer for data analysts. With drag-and-drop ease, it imports CSV files, lets you choose columns, and even apply filters before import. Say goodbye to tedious data structuring and hello to efficient insights extraction. Boost your productivity and streamline your workflow with our user-friendly tool.',
-    },
-    {
-      title: 'Library Management',
-      link: 'https://github.com/vamshidhar644/library_management',
-      client: 'Backend - Node, Express',
-      image: 'library.png',
-      desc: 'Created a secure library management system with user registration, admin access, and book management using NodeJS and MySQL. Developed APIs for adding books, searching, checking availability, and booking with authentication. Ensured admin endpoints are protected with a secret key. System is tested, documented, and deployed for college library operations.',
-    },
-    {
-      title: 'Quizzy App',
-      link: 'https://github.com/vamshidhar644/quizzy-mernstack',
-      client: 'MERN Stack',
-      image: 'quizzy.jpg',
-      desc: "As the team lead for Quizzy, I spearheaded the development of our MERN stack quiz app, and we executed it excellently. The project was a resounding success, reflecting our team's dedication and delivering a high-quality product. I'm incredibly proud of our achievement.",
-    },
-  ];
   return (
-    <div className="projects__parent">
+    <div className="projects__parent gap-8 px-48 py-8">
       <center>
-        <h2>PROJECTS</h2>
+        <h2 className="font-bold text-2xl my-2">PROJECTS</h2>
       </center>
 
       <hr />
-      <div className="projects__container d-flex flex-column">
+      <div className="projects__container d-flex flex-column gap-8">
         {projects.map((project, i) => {
           return (
-            <div className="project__box" key={i}>
+            <div
+              className="project__box grid grid-cols-[7fr_5fr] min-h-[20rem] shadow-[0px_0px_9px_1px_var(--primary-bg)]"
+              key={i}
+            >
               <div className="project_box__1 d-flex justify-content-around flex-column">
-                <div className="project__header px-5">
-                  <h2 className="m-0">{project.title}</h2>
-                  <h3 className="m-0">{project.client}</h3>
-                  <Link to={project.link}>Link</Link>
+                <div className="project__header px-5 border-l-[color:var(--primary-bg)] mx-0 my-[2.4rem] border-l-[12px] border-solid">
+                  <h2 className="m-0 text-[color:var(--secondary-bg)] text-2xl font-medium text-[color:var(--primary-bg)]">
+                    {project.title}
+                  </h2>
+                  <h3 className="m-0 font-semibold text-xl text-[color:var(--secondary-bg)]">
+                    {project.client}
+                  </h3>
+                  <Link
+                    to={project.link}
+                    className="text-[color:var(--primary-bg)] tracking-[1px]"
+                  >
+                    Link
+                  </Link>
                 </div>
 
-                <p className="px-5">{project.desc}</p>
+                <p className="px-5 text-center mb-3">{project.desc}</p>
               </div>
               <div className="project_box__2">
                 <img
